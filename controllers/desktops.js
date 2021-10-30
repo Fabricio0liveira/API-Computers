825f1e947e3b32702064eac2c47507d44e545bdb
fabricio-oliveira <fabriciooh22@gmail.com>
2021-10-30 15:43:45 -0300

    creating computer registration route

--- controllers/desktops.js
+++ controllers/desktops.js
@@ -1,7 +1,13 @@
 module.exports = app => {
-    app.get('/desktops', (req,res) => {
+    app.get('/computers', (req,res) => {
         res.send('Voçê está na rota de computadores e está realizando um GET')
     })
+
+    app.post('/computers', (req, res) => {
+        console.log(req.body)
+        res.send('Voçê está na rota computadores e está enviando um POST')
+    })
 }
+
 
 
