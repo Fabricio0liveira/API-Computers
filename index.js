b09f3b6316c3d97cd3be1bd155a2e46d86931443
fabricio-oliveira <fabriciooh22@gmail.com>
2021-10-30 20:02:23 -0300

    settings for connecting to the database

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const PORT = 3000
 
 // Exporting customExpress
 const customExpress = require('./config/customExpress')
+const conexao = require('./infrastructure/connection')
+
+// connecting to database
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    }
+})
 const app = customExpress()
 
 app.listen(PORT, () => {
