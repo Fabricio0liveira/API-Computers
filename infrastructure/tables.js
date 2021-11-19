b85c3ffd5a8d309844f026f8c109392491522c16
Fabricio Oliveira <fabriciooh22@gmail.com>
2021-11-18 23:43:09 -0300

    adding date to computer registry

--- infrastructure/tables.js
+++ infrastructure/tables.js
@@ -7,7 +7,7 @@ class Tables {
 
     registerComputers() {
         // Create a new table if it does not already exist
-        const sql = 'CREATE TABLE IF NOT EXISTS Computers (id int NOT NULL AUTO_INCREMENT, marca varchar(15) NOT NULL, modelo varchar(25) NOT NULL, patrimonio varchar(8) NOT NULL, status varchar(25) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Computers (id int NOT NULL AUTO_INCREMENT, marca varchar(15) NOT NULL, modelo varchar(25) NOT NULL, patrimonio varchar(8) NOT NULL, status varchar(25) NOT NULL, data datetime NOT NULL, observacoes text, PRIMARY KEY(id))'
         this.connection.query(sql, (erro) => {
             if(erro) {
                 console.log(erro)
@@ -16,9 +16,6 @@ class Tables {
             }
         })
     }
-
-
-
 }
 
 module.exports = new Tables
--- models/desktops.js
+++ models/desktops.js
@@ -1,10 +1,15 @@
+const moment = require('moment')
 const connectionDb = require('../infrastructure/connection')
 
 class Computers {
     add(computer) {
+        // Adjusting the format for entering the date
+        const data = moment(computer.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const updateRecord = {...computer, data}
+
         const sql = 'INSERT INTO Computers SET ?'
 
-        connectionDb.query(sql, computer, (erro, results) => {
+        connectionDb.query(sql, updateRecord, (erro, results) => {
             if(erro) {
                 console.log(erro)
             } else {
